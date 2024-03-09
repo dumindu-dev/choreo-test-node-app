81d36fbac7ddbbbf112364d13046268ebb3b9cf1
Dumindu <dumindu.cse@gmail.com>
2024-03-09 11:33:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.HOST,
+  user     : process.env.USER,
+  password : process.env.PASS,
+  database : process.env.DB
+});
+
+connection.connect();
+
+app.get('/', (req, res) => {
+    connection.query('SELECT name from users', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].name);
+        res.send('Hello World! '+ results[0].name);
+    });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
